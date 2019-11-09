f92150d0663ed5a7dccd64fb27e469e6724e0088
danicodes <danicodes94@gmail.com>
2019-11-09 10:53:18 -0500

    This test doesn't do anything right now

--- test/app_test.py
+++ test/app_test.py
@@ -5,8 +5,7 @@ from flask import Flask, render_template, send_from_directory, request
 import markov_deb.app as app
 #from assets.markov_debate import Debate
 
-app = Flask(__name__)
-
+# TODO: Create working tests :)
 with app.test_request_context('/generate_banter', method='POST'):
     assert request.path == '/generate_banter'
     assert request.method == 'POST'
